2cb5626750434f46ce46aff97d66e1594aafe8b1
Csete Gábor <csgabor88@gmail.com>
2019-11-25 17:24:22 +0100

    added loading and next/prev button functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,59 @@
-let player = document.getElementById('music'); // id for audio element
+let tracks = [{
+        src: "../assets/src/Celldweller - End of an Empire.mp3"
+    },
+    {
+        src: "../assets/src/The Prodigy - Warriors Dance.mp3"
+    },
+    {
+        src: "../assets/src/Noisia - Liliths Club.mp3"
+    }
+];
+
+let currentTrack = 0;
+let player = document.getElementById('music');
+let source = document.getElementsByTagName('source');
 let btnPlayPause = document.getElementById('btnPlayPause');
 let btnMute = document.getElementById('btnMute');
-let progressBar = document.getElementById('progress-bar');
-let volumeBar = document.getElementById('volume-bar');
+let progressBar = document.getElementById('progressBar');
+let volumeBar = document.getElementById('volumeBar');
 
 volumeBar.addEventListener("change", function(evt) {
     player.volume = parseInt(evt.target.value) / 10;
 });
 
-player.addEventListener('timeupdate', updateProgressBar, false);
+player.addEventListener('timeupdate', updateProgressBar);
+progressBar.addEventListener('click', seek);
 
-progressBar.addEventListener("click", seek);
+function load() {
+    currentTrack = 0;
+    progressBar.value = 0;
+    source[0].src = tracks[currentTrack].src;
+    player.load();
+}
+
+function next() {
+    if (currentTrack < tracks.length - 1) {
+        currentTrack++;
+        progressBar.value = 0;
+        source[0].src = tracks[currentTrack].src;
+        player.load();
+        player.play();
+    }
+}
+
+function previous() {
+    if (currentTrack > 0) {
+        currentTrack--;
+        progressBar.value = 0;
+        source[0].src = tracks[currentTrack].src;
+        player.load();
+        player.play();
+    }
+}
 
 function updateProgressBar() {
-    // Work out how much of the media has played via the duration and currentTime parameters
     var percentage = Math.floor((100 / player.duration) * player.currentTime);
-    // Update the progress bar's value
     progressBar.value = percentage;
-    // Update the progress bar's text (for browsers that don't support the progress element)
     progressBar.innerHTML = progressBar.title = percentage + '% played';
 }
 
@@ -30,11 +66,9 @@ function seek(e) {
 
 function playPause() {
     if (player.paused || player.ended) {
-        // Change the button to a pause button
         changeButtonType(btnPlayPause, 'pause');
         player.play();
     } else {
-        // Change the button to a play button
         changeButtonType(btnPlayPause, 'play');
         player.pause();
     }
@@ -42,17 +76,14 @@ function playPause() {
 
 function muteVolume() {
     if (player.muted) {
-        // Change the button to a mute button
         changeButtonType(btnMute, 'mute');
         player.muted = false;
     } else {
-        // Change the button to an unmute button
         changeButtonType(btnMute, 'unmute');
         player.muted = true;
     }
 }
 
-// Updates a button's title, innerHTML and CSS class
 function changeButtonType(btn, value) {
     btn.title = value;
     btn.innerHTML = value;
